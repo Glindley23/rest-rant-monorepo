9628df1f5acc8c12121fb5ea47928f6e0a18f859
Glindley23 <graham.lindley23@gmail.com>
2023-03-12 23:03:24 -0400

    finished JWT auth

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,6 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express();
-
 // Express Settings
 app.use(cors())
 app.use(express.static('public'))
